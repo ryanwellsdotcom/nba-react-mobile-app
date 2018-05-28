311d4d4dddf94ccc6e88dd6430c8eb266802e292
Ryan Wells <ryanwells.com@gmail.com>
2018-05-28 18:33:41 -0400

    getting related videos

---
+++ src/components/widgets/VideoList/VideosRelated/videosRelated.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from '../videoList.css';
+
+import VideoListTemplate from '../videoListTemplate';
+
+const VideosRelated = (props) => {
+  return (
+    <div className={styles.relatedWrapper}>
+      <VideoListTemplate
+        data={props.data}
+        teams={props.teams}
+      />
+    </div>
+  );
+}
+
+export default VideosRelated;
